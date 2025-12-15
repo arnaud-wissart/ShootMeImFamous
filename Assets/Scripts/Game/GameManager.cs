b637a97972534d2d49c985a7bdf3c11df42448ea
Arnaud Wissart <arnaud.wissart@live.fr>
2025-12-15 10:19:01 +0100

    feat: add scoring and spawn safeguards

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -8,6 +8,9 @@ namespace SpaceRocks
         public static GameManager Instance { get; private set; }
 
         private bool _isGameOver;
+        private int _score;
+
+        public int Score => _score;
 
         private void Awake()
         {
@@ -37,14 +40,29 @@ namespace SpaceRocks
 
             _isGameOver = true;
             Time.timeScale = 0f;
-            Debug.Log("Game Over - appuyez sur R pour recommencer");
+            Debug.Log("[GAME] Game Over - appuyez sur R pour recommencer");
         }
 
         public void Restart()
         {
             Time.timeScale = 1f;
             var activeScene = SceneManager.GetActiveScene();
+            Debug.Log("[GAME] Restarting scene");
             SceneManager.LoadScene(activeScene.buildIndex);
+        }
+
+        public void RegisterAsteroidDestroyed(AsteroidSize asteroidSize)
+        {
+            var points = asteroidSize switch
+            {
+                AsteroidSize.Small => 10,
+                AsteroidSize.Medium => 5,
+                AsteroidSize.Large => 2,
+                _ => 0,
+            };
+
+            _score += points;
+            Debug.Log($"[SCORE] +{points} ({asteroidSize}) => total {_score}");
         }
     }
 }
